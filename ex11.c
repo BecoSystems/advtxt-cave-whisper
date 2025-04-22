33a224d655e0f1b61f2897d8bb4c426e38aa9a7c
Jamerson David Da Anunciacao Queiroz <jdaq@poli.br>
2025-04-22 14:04:06 -0300

    Alterando arquivos, falta correções dnv

--- ex11.c
+++ ex11.c
@@ -70,8 +70,10 @@ int main(void){
 
             }
 
-
-
+            if (escolha == 1) printf("A luz da tocha revela armadilhas escondidas. Você pisa em uma placa de pressão, dardos envenenados te acertam, Você morre!!\n FIM DE JOGO!!");
+            if (escolha == 2) printf("A chama atrai uma criatura da caverna. Você só percebe quando ela ja está atras de você, Você morre!! \n FIM DE JOGO!!");
+            if (escolha == 3) printf("Você corre com a tocha acesa. Embora o medo te empurre para longe, você escapa com vida, mas algo te observa do escuro.\n Voce sobreviveu \n FIM DE JOGO, GANHOU!!");
+            return 0;
         }
 
         if (escolha == 2){
@@ -91,8 +93,10 @@ int main(void){
 
             }
 
-
-
+            if (escolha == 1) printf("O espelho reflete uma versão distorcida da caverna. Você segue por onde o reflexo indica... e desaparece, engolido por uma ilusão Você morre!!\n FIM DE JOGO!!");
+            if (escolha == 2) printf("Você vê no reflexo uma sombra se aproximando. Quando se vira, ja é tarde. Tudo fica escuro. Você morre!! \n FIM DE JOGO!!");
+            if (escolha == 3) printf("Você foge e o espelho quebra sozinho em sua mochila. Um alivio estranho te invade, como se tivesse escapado de algo ruim\n Voce sobreviveu!!! \n FIM DE JOGO, GANHOU!!");
+            return 0;
         }
 
         if (escolha == 3){
@@ -112,8 +116,10 @@ int main(void){
 
             }
 
-
-
+            if (escolha == 1) printf("Você lê um feitiço em voz alta. A caverna responde, abrindo o caminho. A aventura so esta começando\n FIM DE JOGO!!\n Você Sobreviveu!!!");
+            if (escolha == 2) printf("O livro começa a flutuar, páginas virando sozinhas. Um portal se abre, chamando por você. Talvez uma oportunidade.. ou um risco\n FIM DE JOGO!!\n Você Sobreviveu!!!");
+            if (escolha == 3) printf("O livro emite um brilho fraco. Ao fugir, você percebe que algo foi deixado para trás, mas vive para descobrir outro dia.\n Voce sobreviveu \n FIM DE JOGO, GANHOU!!");
+            return 0;
         }
 
     }
@@ -125,4 +131,6 @@ int main(void){
 
 
     }
+
+    return 0;
 }
